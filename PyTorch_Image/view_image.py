6f731303b612714773454161a15ad84e875ee3de
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-03-12 14:59:25 +0300

    Create view_image.py

---
+++ PyTorch_Image/view_image.py
@@ -0,0 +1,65 @@
+''' Visualise Image Data '''
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def img_is_color(img):
+
+    if len(img.shape) == 3:
+        # Check the color channels to see if they're all the same.
+        c1, c2, c3 = img[:, : , 0], img[:, :, 1], img[:, :, 2]
+        if (c1 == c2).all() and (c2 == c3).all():
+            return True
+
+    return False
+
+def show_image_list(list_images,       # list of images (npy)
+                    list_titles=None,  # subplot titles
+                    list_cmaps=None,   # cmap for each individual image
+                    grid_id=False,     # turn on/off grid 
+                    axes_id=False,     # turn on/off axes
+                    num_cols=2,        # number of image columns
+                    figsize=(20, 10), title_fontsize=9):
+
+    assert isinstance(list_images, list)
+    assert len(list_images) > 0
+    assert isinstance(list_images[0], np.ndarray)
+
+    if list_titles is not None:
+        assert isinstance(list_titles, list)
+        assert len(list_images) == len(list_titles), '%d imgs != %d titles' % (len(list_images), len(list_titles))
+
+    if list_cmaps is not None:
+        assert isinstance(list_cmaps, list)
+        assert len(list_images) == len(list_cmaps), '%d imgs != %d cmaps' % (len(list_images), len(list_cmaps))
+
+    num_images  = len(list_images)
+    num_cols    = min(num_images, num_cols)
+    num_rows    = int(num_images / num_cols) + (1 if num_images % num_cols != 0 else 0)
+
+    # Create a grid of subplots.
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    
+    # Create list of axes for easy iteration.
+    if isinstance(axes, np.ndarray):
+        list_axes = list(axes.flat)
+    else:
+        list_axes = [axes]
+
+    for i in range(num_images):
+
+        img    = list_images[i]
+        img = rgb_to_gray(img)
+        title  = list_titles[i] if list_titles is not None else 'Image %d' % (i)
+        cmap   = list_cmaps[i] if list_cmaps is not None else (None if img_is_color(img) else 'viridis')
+        
+        list_axes[i].imshow(img, cmap=cmap)
+        list_axes[i].set_title(title, fontsize=title_fontsize) 
+        list_axes[i].grid(grid_id)
+        list_axes[i].axis(axes_id)
+
+    for i in range(num_images, len(list_axes)):
+        list_axes[i].set_visible(False)
+
+    fig.tight_layout()
+    _ = plt.show()
